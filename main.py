aac1d11cefc80f5a6cba1145b6273a0271d972c5
Louis Remes <louisremes@hotmail.be>
2025-05-17 21:36:30 +0200

    Draws player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -13,12 +14,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill([0,0,0])
+        player.draw(screen)
+        
         pygame.display.flip()
 
         dt = Clock.tick(60)/1000
