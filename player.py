7d60e4ecfc8de160f95cad5b8fb504ada9b7e64e
Louis Remes <louisremes@hotmail.be>
2025-05-17 23:15:56 +0200

    Implement reload

--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 from circleshape import CircleShape
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import (PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, 
+                       PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN)
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.reload = 0
 
     # in the player class
     def triangle(self):
@@ -28,6 +30,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.reload -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_d]:
@@ -43,8 +47,10 @@ class Player(CircleShape):
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.reload <= 0:
+                self.shoot()
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.reload = PLAYER_SHOOT_COOLDOWN
